35640de61c7a4aac4d4c285aa9686d264fbcc042
chimemoo <christ.16062@student.unsika.ac.id>
2018-07-24 11:18:35 +0700

    update layout

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -1,7 +1,7 @@
 <!-- jQuery 3 -->
-<script src="<?php echo base_url(); ?>assets/js/jquery/jquery.min.js"></script>
+<script src="<?php echo base_url(); ?>vendor/jquery/jquery.min.js"></script>
 <!-- Bootstrap 3.3.7 -->
-<script src="<?php echo base_url(); ?>assets/css/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url(); ?>vendor/bootstrap/js/bootstrap.min.js"></script>
 <!-- AdminLTE App -->
 <script src="<?php echo base_url(); ?>assets/js/adminlte.min.js"></script>
 </body>
--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -7,11 +7,11 @@
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>vendor/bootstrap/css/bootstrap.min.css">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>vendor/font-awesome/css/font-awesome.min.css">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>vendor/Ionicons/css/ionicons.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/AdminLTE.min.css">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
